efc4d326859ba1c18baf3c532dbc6ac48d8cf4e7
isabel.llanes <isabel.llanes@thougthworks.com>
2022-08-19 13:17:19 +0200

    intStreams exercise. Three different forms to iterate

--- src/test/java/com/amigoscode/examples/IntStreams.java
+++ src/test/java/com/amigoscode/examples/IntStreams.java
@@ -13,15 +13,28 @@ public class IntStreams {
 
     @Test
     public void range() throws Exception {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+        IntStream intStream = IntStream.range(0, 10);
+        System.out.println("inStream exclusive");
+        intStream.forEach(System.out::println);
+
+        IntStream intStreamInclusive = IntStream.rangeClosed(0, 10);
+        System.out.println("inStream inclusive");
+        intStreamInclusive.forEach(System.out::println);
     }
 
-    // Loop through people using IntStream
     @Test
     public void rangeIteratingLists() throws Exception {
         List<Person> people = MockData.getPeople();
+        IntStream.range(0, people.size()).forEach(index -> {
+            System.out.println(people.get(index));
+        });
     }
 
     @Test
-    public void intStreamIterate()  {
+    public void intStreamIterate() {
+        IntStream.iterate(0, value -> value + 1).limit(11).forEach(System.out::println);
     }
 }
